804bf727a926a09505612ae1f16176c47c0990b6
Jacob Edsell <jacobedsell@gmail.com>
2023-10-09 08:54:24 -0700

    working

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use reqwest;
+use scraper::{Html, Selector};
+
+#[feature(async_closure)]
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let url = "https://www.wired.com/";
+    // Make an HTTP GET request
+    let body = reqwest::get(url)
+        .await?
+        .text()
+        .await?;
+    
+    let document = Html::parse_document(&body);
+
+    let selector_result = Selector::parse(r#"script[type="application/ld+json"]"#);
+    let selector = match selector_result {
+        Ok(sel) => sel,
+        Err(err) => {
+            eprintln!("Failed to parse: {:?}", err);
+            return Ok(());
+        }
+    };
+
+    for element in document.select(&selector) {
+        println!("{}", element.text().collect::<String>());
+    }
+                                       
+    Ok(())
 }
